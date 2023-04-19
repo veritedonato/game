7be9366fd57236d439f44062e661329a95db5ea5
veritedonato <veritedonato@gmail.com>
2023-04-19 10:10:44 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,53 +1,7 @@
 // Import stylesheets
 import './style.css';
 
-'use strict';
-let perlin = {
-    rand_vect: function(){
-        let theta = Math.random() * 2 * Math.PI;
-        return {x: Math.cos(theta), y: Math.sin(theta)};
-    },
-    dot_prod_grid: function(x, y, vx, vy){
-        let g_vect;
-        let d_vect = {x: x - vx, y: y - vy};
-        if (this.gradients[[vx,vy]]){
-            g_vect = this.gradients[[vx,vy]];
-        } else {
-            g_vect = this.rand_vect();
-            this.gradients[[vx, vy]] = g_vect;
-        }
-        return d_vect.x * g_vect.x + d_vect.y * g_vect.y;
-    },
-    smootherstep: function(x){
-        return 6*x**5 - 15*x**4 + 10*x**3;
-    },
-    interp: function(x, a, b){
-        return a + this.smootherstep(x) * (b-a);
-    },
-    seed: function(){
-        this.gradients = {};
-        this.memory = {};
-    },
-    get: function(x, y) {
-        if (this.memory.hasOwnProperty([x,y]))
-            return this.memory[[x,y]];
-        let xf = Math.floor(x);
-        let yf = Math.floor(y);
-        //interpolate
-        let tl = this.dot_prod_grid(x, y, xf,   yf);
-        let tr = this.dot_prod_grid(x, y, xf+1, yf);
-        let bl = this.dot_prod_grid(x, y, xf,   yf+1);
-        let br = this.dot_prod_grid(x, y, xf+1, yf+1);
-        let xt = this.interp(x-xf, tl, tr);
-        let xb = this.interp(x-xf, bl, br);
-        let v = this.interp(y-yf, xt, xb);
-        this.memory[[x,y]] = v;
-        return v;
-    }
-}
-perlin.seed();
-
-var objIndex = [];
+var index = [];
 
 function background(color) {
 
@@ -55,60 +9,70 @@ function background(color) {
 
 }
 
-function getRandomInt(max) {
-  return Math.random() * max;
-}
+function player(x,y,name) {
 
-function object (a,b) {
+  var div = document.createElement('div');
 
-var x = 0, y = 0;
+  div.style.position = "absolute";
+  div.style.left = x + "px";
+  div.style.top = y + "px";
+  div.style.width = 64 + "px";
+  div.style.height = 64 + "px";
+  div.style.backgroundColor = "green";
+  div.id = "player_" + name;
 
-while(1) {
-
-    var c = Math.floor((Math.abs(perlin.get(getRandomInt(256),getRandomInt(256))*256)*100000)).toString(16);
-
-    var color = "#" + c;
-
-    console.log(color);
-
-    console.log(a + x);
-    console.log(b + y);
-
-    var div = document.createElement("div");
-
-    div.style.position = "absolute";
-    div.style.left = a + x;
-    div.style.top = b + y;
-    div.style.backgroundColor = color;
-    div.style.width = 64;
-    div.style.height = 64;
-
-    document.body.appendChild(div);
-
-    objIndex.push(div);
-
-    x += 8;
-
-    if ( x >= 64 )
-
-    {
-
-        x = 0;
-
-        y += 8;
-
-        if ( y >= 64 )
-
-            break;
-
-    }
-
-  }
-  
+  document.body.appendChild(div);
 
 }
 
+function monster(x,y,name) {
 
-background("lightgreen");
+  var div = document.createElement('div');
 
-object(250,250);
+  div.style.position = "absolute";
+  div.style.left = x + "px";
+  div.style.top = y + "px";
+  div.style.width = 64 + "px";
+  div.style.height = 64 + "px";
+  div.style.backgroundColor = "red";
+  div.id = "monster_" + name;
+
+  document.body.appendChild(div);
+
+}
+
+function tile(x,y,name,c) {
+
+  var div = document.createElement('div');
+
+  div.style.position = "absolute";
+  div.style.left = x + "px";
+  div.style.top = y + "px";
+  div.style.width = 64 + "px";
+  div.style.height = 64 + "px";
+  div.style.backgroundColor = c;
+  div.id = "tile_" + name;
+
+  document.body.appendChild(div);
+
+
+}
+
+function quest(x,y,name,text) {
+
+  var div = document.createElement('div');
+
+  div.style.position = "absolute";
+  div.style.left = x + "px";
+  div.style.top = y + "px";
+  div.style.width = 64 + "px";
+  div.style.height = 64 + "px";
+  div.style.backgroundColor = "white";
+  div.id = "quest_" + name;
+  div.innerHTML = text;
+
+  document.body.appendChild(div);
+
+}
+
+
