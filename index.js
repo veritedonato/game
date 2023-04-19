559c54d8d873d8fd710d8fc343d42db3a7c8bdd2
veritedonato <veritedonato@gmail.com>
2023-04-19 08:09:10 -0400

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 
+function background(color) {
+
+  appDiv.style.backgroundColor = color;
+
+}
+
+function character() {
+
+  
+}
+
+function object() {
+
+  
+}
+
+function quest() {
+
+  
+}
+
